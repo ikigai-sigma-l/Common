4f34309e4f994a8949807719d097aa9b38d58c25
ikigai-sigma-l <sigma.l@ikigai.team>
2025-04-30 16:42:24 +0800

    add loaiding

--- src/stores/useLoadingStore.ts
+++ src/stores/useLoadingStore.ts
@@ -3,10 +3,12 @@ import { subscribeWithSelector } from 'zustand/middleware'
 
 export interface ILoadingStore {
   percent: number
+  visible: boolean
 }
 
 export const useLoadingStore = create<ILoadingStore>()(
   subscribeWithSelector((set, get) => ({
     percent: 0,
+    visible: true
 }))
 )
---
+++ src/tags/loading/Loading.tsx
@@ -0,0 +1,35 @@
+import { useLoadingStore } from "src/stores/useLoadingStore"
+
+function LoadingBar() {
+    const { percent } = useLoadingStore()
+    const loadPercent = `${(percent * 100).toFixed()}%`
+    return (
+        <div id="ProgressBarContainer">
+            <div id="ProgressBarBG">
+                <div id="ProgressBar" style={{width: loadPercent}}></div>
+            </div>
+            <div id="ProgressText">{loadPercent}</div>
+        </div>
+    )
+}
+
+function LoadingPage() {
+    return (
+        <div id="LoadingPage">
+                <div id="Loader">
+                <div id="LoaderIconContainer">
+                    <img id="LoaderIcon" src="loader/icon.svg" alt="" />
+                </div>
+                <LoadingBar />
+            </div>
+        </div>
+    )
+}
+
+export default function Loading() {
+    const isVisible = useLoadingStore((state) => state.visible)
+
+    return (
+        isVisible && <LoadingPage />
+    )
+}
